4075a7e2ad8d9d516344dbe76c5f135625d942cb
tuong777877 <77165157+tuong777877@users.noreply.github.com>
2022-04-13 17:52:38 +0700

    commit on'master'

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,7 +7,6 @@ import '../widgets/home_category.dart';
 import '../widgets/slider_item.dart';
 import 'dishes.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -15,8 +14,7 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
-
+class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
   List<T> map<T>(List list, Function handler) {
     List<T> result = [];
     for (var i = 0; i < list.length; i++) {
@@ -26,15 +24,12 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
     return result;
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(10.0,0,10.0,0),
+        padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
         child: ListView(
           children: <Widget>[
             Row(
@@ -47,7 +42,6 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                     fontWeight: FontWeight.w800,
                   ),
                 ),
-
                 FlatButton(
                   child: Text(
                     "View More",
@@ -57,10 +51,10 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                       color: Theme.of(context).colorScheme.secondary,
                     ),
                   ),
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
-                        builder: (BuildContext context){
+                        builder: (BuildContext context) {
                           return const DishesScreen();
                         },
                       ),
@@ -75,18 +69,19 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
             //Slider Here
 
             CarouselSlider(
-              options: CarouselOptions(height: MediaQuery.of(context).size.height/2.4,
-                  autoPlay: true, //enlargeCenterPage: true,
-                  viewportFraction: 1.0, //aspectRatio: 2.0,
-              //   onPageChanged: (index){ setState(() {
-              //   _current = index;
-              // }),
-              // }
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height / 2.4,
+                autoPlay: true, //enlargeCenterPage: true,
+                viewportFraction: 1.0, //aspectRatio: 2.0,
+                //   onPageChanged: (index){ setState(() {
+                //   _current = index;
+                // }),
+                // }
               ),
               items: map<Widget>(
                 foods,
-                    (index, i){
-                      Map food = foods[index];
+                (index, i) {
+                  Map food = foods[index];
                   return SliderItem(
                     img: food['img'],
                     isFav: false,
@@ -114,7 +109,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                 scrollDirection: Axis.horizontal,
                 shrinkWrap: true,
                 // ignore: unnecessary_null_comparison
-                itemCount: categories == null?0:categories.length,
+                itemCount: categories == null ? 0 : categories.length,
                 itemBuilder: (BuildContext context, int index) {
                   Map cat = categories[index];
                   return HomeCategory(
@@ -122,8 +117,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                     title: cat['name'],
                     items: cat['items'].toString(),
                     isHome: true,
-                    tap: (){ },
-
+                    tap: () {},
                   );
                 },
               ),
@@ -141,7 +135,6 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                     fontWeight: FontWeight.w800,
                   ),
                 ),
-
                 FlatButton(
                   child: Text(
                     "View More",
@@ -151,7 +144,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                       color: Theme.of(context).colorScheme.secondary,
                     ),
                   ),
-                  onPressed: (){},
+                  onPressed: () {},
                 ),
               ],
             ),
@@ -167,7 +160,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home>{
                     (MediaQuery.of(context).size.height / 1.25),
               ),
               // ignore: unnecessary_null_comparison
-              itemCount: foods == null ? 0 :foods.length,
+              itemCount: foods == null ? 0 : foods.length,
               itemBuilder: (BuildContext context, int index) {
 //                Food food = Food.fromJson(foods[index]);
                 Map food = foods[index];
